a5e771af243572dcd3dc07520da3f9c363b9dcd3
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-07 01:13:59 +0200

    Integration test for process inductor endpoint.

---
+++ carnifex/test/integration/test_localprocess_endpoint.py
@@ -0,0 +1,46 @@
+from twisted.trial.unittest import TestCase
+from carnifex.inductor import ProcessInductor
+from carnifex.localprocess import ReactorProcessFactory
+from twisted.internet.error import ConnectionDone
+from twisted.internet import protocol, reactor, defer
+from carnifex.endpoint import InductorEndpoint
+
+
+class InductorEndpointTest(TestCase):
+    """Test connecting a twisted protocol to a process.
+    """
+
+    def test_real_endpoint(self):
+        processFactory = ReactorProcessFactory(reactor)
+        inductor = ProcessInductor(processFactory)
+        executable = 'echo'
+        echo_text = "hello world!"
+        expected_data = echo_text + '\n'
+        #TODO: fix timeout
+        endpoint = InductorEndpoint(inductor, executable, args=(executable, echo_text))
+
+        dataDeferred = defer.Deferred()
+        connDeferred = defer.Deferred()
+        class MockProtocol(protocol.Protocol):
+            def dataReceived (self, data):
+                dataDeferred.callback(data)
+            def connectionLost (self, reason):
+                connDeferred.callback(reason)
+
+        protocolFactory = protocol.ClientFactory()
+        protocolFactory.protocol = MockProtocol
+        clientDeferred = endpoint.connect(protocolFactory)
+
+        @clientDeferred.addCallback
+        def check_client(client):
+            self.assertIsInstance(client, MockProtocol)
+
+        @dataDeferred.addCallback
+        def check_received_data(data):
+            return self.assertEqual(data, expected_data)
+
+        # Check that the connectionLost reason
+        self.assertFailure(connDeferred, ConnectionDone)
+
+        return defer.DeferredList([clientDeferred, dataDeferred, connDeferred],
+                                  fireOnOneErrback=True)
