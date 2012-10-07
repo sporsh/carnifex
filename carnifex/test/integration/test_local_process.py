40aa16d48c58a17ab08ac526e1a8806214167a1b
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-08 01:02:19 +0200

    Add more tests to the local process integration test

--- carnifex/test/integration/test_local_process.py
+++ carnifex/test/integration/test_local_process.py
@@ -16,3 +16,34 @@ class InductorTest(TestCase):
             expected_result = (expected_stdout, '', 0)
             self.assertEqual(result, expected_result)
         return result
+
+    def test_real_run_unknown_command(self):
+        executable = 'thiscommandshouldnotexist'
+
+        inductor = LocalProcessInductor(reactor)
+        result = inductor.run(executable)
+        @result.addCallback
+        def check_result(result):
+            stdout, stderr, code = result
+            self.assertEqual(stdout, '')
+            self.assertNotEqual(stderr, '')
+            self.assertNotEqual(code, 0)
+        return result
+
+    def test_getExitStatus_false(self):
+        inductor = LocalProcessInductor(reactor)
+        result = inductor.getExitStatus('false')
+        @result.addCallback
+        def check_result(result):
+            self.assertNotEqual(result, 0, "The 'false' command should "
+                                "exit with a nonzero code")
+        return result
+
+    def test_getExitStatus_true(self):
+        inductor = LocalProcessInductor(reactor)
+        result = inductor.getExitStatus('true')
+        @result.addCallback
+        def check_result(result):
+            self.assertEqual(result, 0, "The 'true' command should "
+                             "exit with code 0")
+        return result
