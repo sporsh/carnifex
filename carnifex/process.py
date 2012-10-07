c91ba342c15c283724982220391d5fe44cd51428
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-08 01:04:09 +0200

    Remove stale file from when we used a process factory

--- carnifex/process.py
+++
@@ -1,6 +0,0 @@
-class ProcessFactory(object):
-    """Class that encapsulate functionality to spawn processes
-    """
-    def spawnProcess(self, processProtocol, executable, args=(), env={},
-                     path=None, uid=None, gid=None, usePTY=0, childFDs=None):
-        raise NotImplementedError()
