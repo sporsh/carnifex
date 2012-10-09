9ad37072631aa936f78a28eb87bbe2b4b674900a
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-10 00:23:15 +0200

    Add integration test for endpoint with ssh process inductor and combine with local process inductor test.

--- carnifex/test/integration/test_endpoint.py
+++ carnifex/test/integration/test_endpoint.py
@@ -1,8 +1,15 @@
 from twisted.trial.unittest import TestCase
 from carnifex.localprocess import LocalProcessInductor
 from twisted.internet.error import ConnectionDone
-from twisted.internet import protocol, reactor, defer
+from twisted.internet import protocol, defer
 from carnifex.endpoint import InductorEndpoint
+from carnifex.sshprocess import SSHProcessInductor
+
+
+UID = None # indicate that we want to run processes as the current user.
+ECHO_COMMAND = 'echo' # 'echo` should echo back on stdout
+# `echo` return an additional newline after the given text to echo.
+echoTransform = lambda text: text + '\n'
 
 
 class GatherProtocol(protocol.Protocol):
@@ -17,19 +24,16 @@ class GatherProtocol(protocol.Protocol):
         self.resultDeferred.callback((self.gatheredData, reason))
 
 
-class InductorEndpointTest(TestCase):
+class InductorEndpointTestMixin(object):
     """Test connecting a twisted protocol to a process.
     """
-
     def test_real_endpoint(self):
-        executable = 'echo'
         echoText = "hello world!"
-        expectedGatheredData = [echoText + '\n']
+        expectedGatheredData = [echoTransform(echoText)]
         expectedConnectionLostReason = ConnectionDone
 
-        inductor = LocalProcessInductor(reactor)
-        endpoint = InductorEndpoint(inductor, executable=executable,
-                                    args=(executable, echoText), timeout=1)
+        endpoint = InductorEndpoint(self.inductor, executable=ECHO_COMMAND,
+                                    args=(ECHO_COMMAND, echoText), timeout=1)
 
         protocolFactory = protocol.ClientFactory()
         protocolFactory.protocol = GatherProtocol
@@ -49,3 +53,21 @@ class InductorEndpointTest(TestCase):
         return self.assertFailure(clientDeferred, expectedConnectionLostReason)
 
         return clientDeferred
+
+
+class LocalProcessInductorEndpointTest(TestCase, InductorEndpointTestMixin):
+    def setUp(self):
+        from twisted.internet import reactor
+        self.inductor = LocalProcessInductor(reactor)
+
+
+class SSHInductorEndpointTest(TestCase, InductorEndpointTestMixin):
+    def setUp(self):
+        from twisted.internet import reactor
+        host, port = 'localhost', 22
+        self.inductor = SSHProcessInductor(reactor, host, port)
+
+    def tearDown(self):
+        inductor = self.inductor
+        self.inductor = None
+        inductor.disconnectAll()
