625a3c6e8d67af5093867ac5f7ee435f8b2d1210
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-11 03:31:52 +0200

    Added a Distutils setup.py file

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+readme_md = open('README.md', 'r').read()
+
+setup(
+      name='carnifex',
+      version='0.1.0',
+      packages=['carnifex'],
+      provides=['carnifex'],
+      requires=['Twisted'],
+
+      author='Geir Sporsheim',
+      author_email='gksporsh@gmail.com',
+      url='http://github.com/sporsh/carnifex',
+
+      description="Provides a uniform way of running processes remotely and locally.",
+      classifiers=[
+                   'Programming Language :: Python',
+                   'Development Status :: 3 - Alpha',
+                   'Framework :: Twisted',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Information Technology',
+                   'Intended Audience :: System Administrators',
+                   'Operating System :: OS Independent',
+                   'Topic :: Internet',
+                   'Topic :: Security',
+                   'Topic :: System',
+                   'Topic :: System :: Networking',
+                   'Topic :: System :: Operating System',
+                   'Topic :: System :: Systems Administration',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Terminals'
+                   ],
+      long_description=readme_md
+      )
