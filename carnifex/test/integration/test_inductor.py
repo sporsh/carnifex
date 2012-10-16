f91a19585bf1e127ee7f7437bd4f3352d7320fdc
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-17 00:17:32 +0200

    Syntactical cleanup of test case method

--- carnifex/test/integration/test_inductor.py
+++ carnifex/test/integration/test_inductor.py
@@ -43,7 +43,6 @@ class InductorTestMixin(object):
     def test_run_stdout_stderr_exit(self):
         """Check that we get the expected stdout, stderr and exit code
         """
-        executable = PYTHON_COMMAND
         stdoutText = "output out o text"
         stderrText = "error err e text"
         exitCode = 47
@@ -52,12 +51,12 @@ class InductorTestMixin(object):
                         "sys.stderr.write('%s');"
                         "exit(%i);"
                         % (stdoutText, stderrText, exitCode))
-        args = (executable, '-c', pythonScript)
+        args = (PYTHON_COMMAND, '-c', pythonScript)
 
         disconnectedDeferred = defer.Deferred()
         protocol = ProcessProtocol()
         protocol.processEnded = disconnectedDeferred.callback
-        resultDeferred = self.inductor.run(executable, args, uid=UID)
+        resultDeferred = self.inductor.run(args[0], args, uid=UID)
         @resultDeferred.addCallback
         def checkResult((r_stdoutText, r_stderrText, r_exitCode)):
             self.assertEqual(r_stdoutText, stdoutText, "stdout not as expected")
