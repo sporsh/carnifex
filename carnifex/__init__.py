df143fdca3dcbd29000a5b4ff0fadbbc8a76a33b
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-17 00:16:37 +0200

    Elevate exposure of the most interesting classes

---
+++ carnifex/__init__.py
@@ -0,0 +1,9 @@
+# Inductor implementations
+from localprocess import LocalProcessInductor
+from sshprocess import SSHProcessInductor
+
+# Inductor endpoint
+from endpoint import InductorEndpoint
+
+# Utility methods
+from commandline import parseCommandLine
