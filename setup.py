512f5c742d4506de14365588053c3b2b2ea47f10
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-17 21:30:43 +0200

    Use static long_description for setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from distutils.core import setup
 
-readme = open('README.rst', 'r').read()
-
 setup(
       name='carnifex',
       version='0.1.0',
@@ -13,7 +11,7 @@ setup(
       author_email='gksporsh@gmail.com',
       url='http://github.com/sporsh/carnifex',
 
-      description="Provides a uniform way of running processes remotely and locally.",
+      description="Execute commands and run processes locally and remote",
       classifiers=[
                    'Programming Language :: Python',
                    'Development Status :: 3 - Alpha',
@@ -31,5 +29,15 @@ setup(
                    'Topic :: Software Development :: Libraries :: Python Modules',
                    'Topic :: Terminals'
                    ],
-      long_description=readme
+      long_description="""\
+Carnifex
+========
+
+Carnifex provides an api for executing commands.
+The processes can be started locally or remotely on another machine with
+minimal effort for the user.
+
+The module builds on Twisted and uses Twisted Conch for executing commands
+on a remote machine.
+"""
       )
