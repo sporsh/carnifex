33b1397c4aecdc9f9867f0156dfc5c7d9ad932d3
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-20 15:30:36 +0200

    Classes for abstracting commands

--- carnifex/commandline.py
+++ carnifex/commandline.py
@@ -1,4 +1,6 @@
 import shlex
+import pipes
+from utils import attr_string
 
 def parseCommandLine(commandLine, **kwargs):
     """Utility method for generating arguments for executing a command.
@@ -12,3 +14,54 @@ def parseCommandLine(commandLine, **kwargs):
     kwargs['executable'] = args[0]
     kwargs['args'] = args
     return kwargs
+
+
+class Command(object):
+    def __init__(self, command):
+        """
+        @param command: A command object (eg. a PosixCommand) or string
+        """
+        self.command = command
+
+    def getCommandLine(self):
+        return str(self.command)
+
+    def __str__(self):
+        return self.getCommandLine()
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, attr_string(**self.__dict__))
+
+
+class PosixCommand(Command):
+    fixArgs = True
+
+    def __init__(self, executable, *args):
+        self.executable = executable
+        self.args = list(args) or [executable]
+
+        # Make sure args[0] is the executable if the fixArgs flag is set:
+        if self.fixArgs and args and not executable in args[0]:
+            self.args.insert(0, executable)
+
+    def getCommandLine(self):
+        """Correctly quote the arguments and concatenate into one command line
+        """
+        return ' '.join(pipes.quote(arg) for arg in self.args)
+
+    def __str__(self):
+        return self.getCommandLine()
+
+
+class SSHCommand(Command):
+    def getCommandLine(self, precursor=None, path=None, cd='cd', sep=';'):
+        """
+        @param precursor: Precursor to the command line (eg. 'source /etc/profile')
+        @param cd: The command used to change working directory (usually 'cd')
+        @param sep: Syntax for separating several commands in one command line.
+                    Usually '&&' or ;' for bash like command lines.
+        """
+        commandLine = precursor + sep if precursor else ''
+        commandLine += cd + ' ' + path + sep if path else ''
+        commandLine += str(self.command)
+        return commandLine
