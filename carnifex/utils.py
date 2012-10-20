7a2765f9d5209eece93aca0ecd08781d2f844a88
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-20 15:28:36 +0200

    Convenience method for generating string from arguments

---
+++ carnifex/utils.py
@@ -0,0 +1,2 @@
+def attr_string(filterKeys=(), filterValues=(), **kwargs):
+    return ', '.join([str(k)+'='+repr(v) for k, v in kwargs.iteritems() if k not in filterKeys and v not in filterValues])
