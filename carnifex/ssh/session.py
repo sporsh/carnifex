8e999be4b8bd05cc62b918705dad48dc99e13008
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-27 01:56:53 +0200

    Have SSHSession implement IProcessTransport

--- carnifex/ssh/session.py
+++ carnifex/ssh/session.py
@@ -1,20 +1,58 @@
 import struct
 from twisted.python import failure
 from twisted.internet.error import ProcessTerminated, ProcessDone
-from twisted.conch.ssh import connection
+from twisted.conch.ssh import common, connection
 from twisted.conch.ssh.channel import SSHChannel
+from twisted.internet.interfaces import IProcessTransport
+from zope.interface.declarations import implements
 
 
 class SSHSession(SSHChannel):
+    implements(IProcessTransport)
+
     name = 'session'
     exitCode = None
     signal = None
     status = -1
+    pid = None
 
     def __init__(self, deferred, protocol, *args, **kwargs):
         SSHChannel.__init__(self, *args, **kwargs)
         self.deferred = deferred
         self.protocol = protocol
+
+    def closeStdin(self):
+        self.closeChildFD(0)
+
+    def closeStdout(self):
+        self.closeChildFD(1)
+
+    def closeStderr(self):
+        self.closeChildFD(2)
+
+    def closeChildFD(self, descriptor):
+        if descriptor == 0:
+            self.conn.sendEOF(self)
+
+    def writeToChild(self, childFD, data):
+        if childFD == 0:
+            self.write(data)
+
+    def loseConnection(self):
+        self.closeStdin()
+        self.closeStderr()
+        self.closeStdout()
+        SSHChannel.loseConnection(self)
+
+    def signalProcess(self, signal):
+        """Deliver a signal to the remote process/service.
+
+        @param signal: signal name (without the "SIG" prefix)
+        @warning: Some systems may not implement signals,
+                  in which case they SHOULD ignore this message.
+        """
+        signal = common.NS(signal)
+        return self.conn.sendRequest(self, 'signal', signal, wantReply=True)
 
     def channelOpen(self, specificData):
         # Connect the SSHSessionProcessProtocol
