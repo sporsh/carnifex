58148ae2a0934b422b181e568331ea19282a84bf
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-10-27 02:14:39 +0200

    Subclassed SSHClientTransport to support premature disconnects (e.g. auth failures)

--- carnifex/ssh/client.py
+++ carnifex/ssh/client.py
@@ -1,5 +1,19 @@
 from twisted.internet.protocol import ClientFactory
 from twisted.conch.ssh.transport import SSHClientTransport
+from twisted.internet.error import ConnectionClosed
+
+
+class DisconnectError(ConnectionClosed):
+    def __init__(self, reasonCode, description):
+        ConnectionClosed.__init__(self, description)
+        self.reasonCode = reasonCode
+        self.description = description
+
+class TooManyAuthFailures(DisconnectError):
+    """Too many authentication failures for user
+    """
+
+disconnectErrors = {2: TooManyAuthFailures}
 
 
 class SSHClientFactory(ClientFactory):
@@ -12,3 +26,25 @@ class SSHClientFactory(ClientFactory):
         trans.verifyHostKey = self.verifyHostKey
         trans.connectionSecure = lambda: trans.requestService(self.userAuthObject)
         return trans
+
+
+class SSHTransport(SSHClientTransport):
+    def __init__(self, deferred, userAuthObject, verifyHostKey):
+        self.deferred = deferred
+        self.userAuthObject = userAuthObject
+        self.verifyHostKey = verifyHostKey
+
+    def receiveError(self, reasonCode, description):
+        error = disconnectErrors.get(reasonCode, DisconnectError)
+        self.connectionClosed(error(reasonCode, description))
+        SSHClientTransport.receiveError(self, reasonCode, description)
+
+    def connectionLost(self, reason):
+        SSHClientTransport.connectionLost(self, reason)
+        self.connectionClosed(reason)
+
+    def connectionSecure(self):
+        self.requestService(self.userAuthObject)
+
+    def connectionClosed(self, reason):
+        self.deferred.called or self.deferred.callback(reason)
