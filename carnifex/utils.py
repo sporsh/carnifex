28a457926921ef5e7f57c086d6e6b77a0221348c
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-11-16 00:28:47 +0100

    Add documentation and touch up formatting of attr_string util method

--- carnifex/utils.py
+++ carnifex/utils.py
@@ -4,4 +4,12 @@
 """
 
 def attr_string(filterKeys=(), filterValues=(), **kwargs):
-    return ', '.join([str(k)+'='+repr(v) for k, v in kwargs.iteritems() if k not in filterKeys and v not in filterValues])
+    """Build a string consisting of 'key=value' substrings for each keyword
+    argument in :kwargs:
+
+    @param filterKeys: list of key names to ignore
+    @param filterValues: list of values to ignore (e.g. None will ignore all
+                         key=value pairs that has that value.
+    """
+    return ', '.join([str(k)+'='+repr(v) for k, v in kwargs.iteritems()
+                      if k not in filterKeys and v not in filterValues])
